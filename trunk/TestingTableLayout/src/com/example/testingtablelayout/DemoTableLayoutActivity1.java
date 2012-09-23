a04f4cc89620770d33700f1d6d1f9bcad0fe5ad3
tiagotele@gmail.com <tiagotele@gmail.com@e6c2f30a-7b83-b1a9-3e53-fb1c6d5d4822>
2012-09-23 18:43:50 +0000

    First commit of this project.

---
+++ trunk/TestingTableLayout/src/com/example/testingtablelayout/DemoTableLayoutActivity1.java
@@ -0,0 +1,16 @@
+package com.example.testingtablelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoTableLayoutActivity1 extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.demo_table_layout1);
+	}
+}
